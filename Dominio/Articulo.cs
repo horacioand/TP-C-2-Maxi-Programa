b8a7abb4fae36718a2a32961093f5cd72653d165
Horacio <horacioanadon00@gmail.com>
2024-12-22 22:39:17 -0300

    creacion clases principales dominio, negocio y visual, desarrollo modelo de dominio

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    //Los datos mínimos con los que deberá contar el artículo son los siguientes:
+
+    //Código de artículo.
+    //Nombre.
+    //Descripción.
+    //Marca(seleccionable de una lista desplegable).
+    //Categoría(seleccionable de una lista desplegable.
+    //Imagen.
+    //Precio.
+    public class Articulo
+    {
+        public int Id;
+        public string CodigoArticulo;
+        public string Nombre;
+        public string Descripcion;
+        public Marca Marca;
+        public Categoria Categoria;
+        public string Imagen;
+        public float Precio;
+    }
+}
